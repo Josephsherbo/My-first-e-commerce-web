545465af6a926170539751ed8da4668cf4d6bc11
youssef sherbo <youssefmohammadsherbo2003@gmail.com>
2022-04-26 21:13:36 +0200

    .

--- fec/script.js
+++ fec/script.js
@@ -1,12 +1,12 @@
-const navslide = () => {
+ 	const navslide = () => {
 	const burger = document.querySelection('.burger');
 	const nav = document.querySelection('.nav-links');
 	const navlinks = document.querySelectorAll('.nav-links li');
-	//toggle nav
+	
 	burger.addEventListener('click', () => {
 		nav.class.toggle('nav-active');
 	});
-	// animate links
+	
 	navlinks.forEach((link, index)=>{
 		link.style.animation = 'navlinkfade 0.5s ease forwards ${index / 7}s';
 		console.log(index / 7);
