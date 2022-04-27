5322ffdb02e492d40e6d28da5bea466994ce1aed
youssef sherbo <youssefmohammadsherbo2003@gmail.com>
2022-04-27 15:38:45 +0200

    navbar commit

--- datalog.md
+++ datalog.md
@@ -2,19 +2,22 @@
 this is the first web that i am going to make for someone.
  
  first import 24/4/2022:
- 
  -created project.
- 
  -imported:
  --index.html 
- --style.css
- 
+ --style.css 
+----------------------------------------
  seond import 26/4/2022:
-
 -updated:
  --index.html 
  --style.css
- 
  -imported:
  --images.
+ --script.js
+-----------------------------------------
+third import 27/4/2022:
+-finished navbar.
+-updated:
+ --index.html 
+ --style.css
  --script.js
--- fec/script.js
+++ fec/script.js
@@ -1,16 +1,22 @@
- 	const navslide = () => {
-	const burger = document.querySelection('.burger');
-	const nav = document.querySelection('.nav-links');
+const navslide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
 	const navlinks = document.querySelectorAll('.nav-links li');
-	
+
 	burger.addEventListener('click', () => {
-		nav.class.toggle('nav-active');
-	});
+		nav.classList.toggle('nav-active');
 	
-	navlinks.forEach((link, index)=>{
-		link.style.animation = 'navlinkfade 0.5s ease forwards ${index / 7}s';
-		console.log(index / 7);
+		navlinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navlinkfade 0.5s ease forwards ${index / 7 + 0.5}s`;
+			}
+		});
+		//burger animation
+		burger.classList.toggle('toggle');
 	});
+
 }
 
 navslide();
